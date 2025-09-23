809af3545ee6074f6cb8edc0a5bffe4fd1a028af
Ayesta1 <ayesta1@gmail.com>
2025-09-23 13:16:27 +0200

    1.0 - Creacion inicial de la clase Jugados.cs con sus atributos y constructor solo

---
+++ ClubJoxe/Jugador.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClubJoxe
+{
+    public enum ePosicion
+    {
+        P,
+        DF,
+        MC,
+        D
+    }
+    internal class Jugador
+    {
+        private int dorsal;
+        private string nombre;
+        private ePosicion posicion;
+
+        public int Dorsal { get { return dorsal; } set { dorsal = value; } }
+        public string Nombre { get { return nombre; } set { nombre = value; } }
+
+        public ePosicion Posicion { get { return posicion; } private set { posicion = value; } }
+        public Jugador( int dorsal, string nombre, ePosicion posicion) 
+        {
+            this.Dorsal = dorsal;
+            this.Nombre = nombre;
+            this.Posicion = posicion;
+
+        }
+    }
+}
--- ClubJoxe/Program.cs
+++ ClubJoxe/Program.cs
@@ -10,6 +10,7 @@ namespace ClubJoxe
     {
         static void Main(string[] args)
         {
+
         }
     }
 }
